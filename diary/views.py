1afcfc2db03c77d796527d34a897d8fe2b431c28
unknown <steveny@KILKENNY.ryarc.corp>
2012-05-04 16:57:45 +1000

    CSS changes. Paging implemented.

--- diary/views.py
+++ diary/views.py
@@ -8,15 +8,22 @@ from diary.forms import CreateGreetingForm
 from diary.models import Greeting
 
 MEMCACHE_GREETINGS = 'greetings'
+items_per_page = 10
 
 def list_greetings(request):
-    greetings = cache.get(MEMCACHE_GREETINGS)
+    page_number = 1
+    try:
+        page_number = int(request.GET.get('p','1'))
+    except ValueError:
+        page_number = 1
+    greetings = cache.get(MEMCACHE_GREETINGS + str(page_number))
     if greetings is None:
-        greetings = Greeting.objects.all().order_by('-date')[:10]
-        cache.add(MEMCACHE_GREETINGS, greetings)
+        greetings = Greeting.objects.all().order_by('-date')[(page_number-1)*items_per_page:page_number*items_per_page]
+        cache.add(MEMCACHE_GREETINGS + str(page_number), greetings)
     return direct_to_template(request, 'diary/index.html',
                               {'greetings': greetings,
-                               'form': CreateGreetingForm()})
+                               'form': CreateGreetingForm(),
+                               'nextpage': page_number + 1})
 
 def create_greeting(request):
     if request.method == 'POST' and request.user.is_authenticated():
@@ -26,7 +33,8 @@ def create_greeting(request):
             if request.user.is_authenticated():
                 greeting.author = request.user
             greeting.save()
-            cache.delete(MEMCACHE_GREETINGS)
+            for i in range(1, Greeting.objects.all().count() / items_per_page + 2):
+                cache.delete(MEMCACHE_GREETINGS + str(i))
     return HttpResponseRedirect('/diary/')
 
 def login(request):
--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/diary/', }),
-    (r'^diary/', include('diary.urls')),
-
+    (r'^diary/?', include('diary.urls')),
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/media/favicon.ico'}),
     (r'^accounts/', include('gaeauth.urls')),
     (r'^invalid', 'django.views.generic.simple.direct_to_template',{'template':'invalid.html'}),
     #(r'.+\.html$', 'django.views.generic.simple.direct_to_template'),
