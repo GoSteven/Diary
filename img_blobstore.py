7ce508afd754464e224f50dbab2f9f546ad10684
Steven You <yousilin@gmail.com>
2012-06-16 00:25:43 +1000

    Upload photo using blobstore

---
+++ img_blobstore.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+
+import os
+import urllib
+
+from google.appengine.ext import blobstore
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/img/upload')
+        self.response.out.write('<html><body>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+        name="submit" value="Submit"> </form></body></html>""")
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        self.redirect('/img/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
+
+def main():
+    application = webapp.WSGIApplication(
+        [('/img/', MainHandler),
+            ('/img/upload', UploadHandler),
+            ('/img/serve/([^/]+)?', ServeHandler),
+        ], debug=True)
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
